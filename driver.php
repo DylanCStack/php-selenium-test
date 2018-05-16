b7ff5dd0c6aad6bb084bc37b987d9a344536409f
Dylan Stackhouse <Stackhouse.Dylan@gmail.com>
2018-05-15 18:36:27 -0700

    create php script to start a server and get the homepage with selenium

---
+++ driver.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once('vendor/autoload.php');
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\WebDriverBy;
+
+$baseUrl = 'localhost:8000';
+
+// see if the selenium executable exists, if not download it because what's the point of automation if you have to do it yourself.
+if(!glob('selenium.jar')) {
+  file_put_contents(__DIR__.'/selenium.jar', file_get_contents('https://goo.gl/tbd1NS'));
+}
+
+// start the selenium server in the background
+exec('java -jar selenium.jar > /dev/null &');
+
+// start a simple web server in the background
+exec('php -S '.$baseUrl.' > /dev/null &');
+
+$host = 'http://localhost:4444/wd/hub';
+$driver = RemoteWebDriver::create($host, DesiredCapabilities::chrome(), 5000);
+
+$driver->get($baseUrl);
+
+$driver->quit();
+exec('lsof -t -i :4444 | xargs kill > /dev/null &');
+exec('killall php > /dev/null &');
+
